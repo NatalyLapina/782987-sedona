08fead9207ef13f1f0307ffa72f83715cf95874d
NatalyLapina <sumrakn@gmail.com>
2018-07-02 23:59:27 +0300

    Удаление  localStorage

--- js/index.js
+++ js/index.js
@@ -4,12 +4,6 @@ var popup = document.querySelector(".search-modal");
 
 var checkin = popup.querySelector("[name=checkin]");
 
-var form = popup.querySelector(".search-form");
-var adult = popup.querySelector("[name=adult]");
-var child = popup.querySelector("[name=child]");
-var storageA = localStorage.getItem("adult");
-var storageC = localStorage.getItem("child");
-
 document.documentElement
     .classList.replace('no-js', 'js');
         
@@ -17,13 +11,4 @@ btn.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
     checkin.focus();
-    if (storageA || storageC) {
-        adult.value = storageA;
-        child.value = storageC;
-    }
 });      
-
- form.addEventListener("submit", function (evt) {
-    localStorage.setItem("adult", adult.value); 
-    localStorage.setItem("child", child.value); 
-});
